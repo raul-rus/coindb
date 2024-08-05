4991ec126ba0d73e5355cf23dd4c5ccc33c058f6
Raul Rus <raul.victor.rus@gmail.com>
2024-08-05 07:00:19 +0100

    Added AJAX and JSON calls to retrieve and overwrite the database for the add_coin page.

--- app.py
+++ app.py
@@ -8,30 +8,24 @@ app = Flask(__name__)
 ACCESS = 'http://127.0.0.1:8000/hello'
 ROOT = '/Users/raulrus/coindb-data'
 DATABASE_FILE_PATH = os.path.join(ROOT, 'coin_database.txt')
-
+f = open(DATABASE_FILE_PATH, "r")
+data = json.loads(f.read())
+f.close()
 
 def create_database():
-    # create a dataframe
-    data = {
-        "id": [],
-        "denomination": [],
-        "region": [],
-        "year": [],
-        "currency": [],
-        "metal": [],
-        "diameter": []
-    }
-    db = pd.DataFrame(data, index=[])
-    print(db)
-
+    data.append(["id", "denomination", "region", "year", "currency", "metal", "diameter"])
     f = open(DATABASE_FILE_PATH, "w")
-    print(DATABASE_FILE_PATH)
-    f.write(db.to_json())
-    print(db.to_json())
+    f.write(json.dumps(data))
     f.close()
 
-create_database()
-sys.exit(0)
+def add_new_data(denomination, region, year, currency, metal, diameter):
+    data.append([len(data), denomination, region, year, currency, metal, diameter])
+    f = open(DATABASE_FILE_PATH, "w")
+    f.write(json.dumps(data))
+    f.close()
+
+def overwrite_data(id, denomination, region, year, currency, metal, diameter):
+    data[id] = [id, denomination, region, year, currency, metal, diameter]
 
 
 @app.route("/show")
@@ -42,6 +36,13 @@ def show():
     denominations = [coin['denomination'] for coin in coins]
     return content
 
+# parameters flask
+@app.route("/add", methods=['POST'])
+def add():
+    features = request.get_json()
+    add_new_data(*features)
+    return {'response': "Coin added"}
+
 @app.route("/catalogue")
 def catalogue():
     return render_template('catalogue.html')
@@ -49,6 +50,16 @@ def catalogue():
 @app.route("/display_collection")
 def display_collection():
     return render_template('display_collection.html')
+@app.route("/dummy")
+def dummy():
+    return 'DUMMY'
+
+@app.route("/coins")
+def coins():
+    f = open(DATABASE_FILE_PATH, "r")
+    result = f.read()
+    f.close()
+    return result
 
 @app.route("/add_coin")
 def add_coin():
