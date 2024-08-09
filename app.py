93afc7ca69b380a7758698b4e23692dda87bd6ee
Raul Rus <raul.victor.rus@gmail.com>
2024-08-09 17:43:29 +0100

    Added a method to display coin data in a table with separate js and css files

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 import sys
 from flask import Flask, request, render_template
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static',)
 
 ACCESS = 'http://127.0.0.1:8000/hello'
 ROOT = '/Users/raulrus/coindb-data'
@@ -11,6 +11,12 @@ DATABASE_FILE_PATH = os.path.join(ROOT, 'coin_database.txt')
 f = open(DATABASE_FILE_PATH, "r")
 data = json.loads(f.read())
 f.close()
+
+def find_id(denomination, region, year, currency, metal, diameter):
+    f = open(DATABASE_FILE_PATH, "r")
+    data = json.loads(f.read())
+    # parse through data and find the id
+    return -1
 
 def create_database():
     data.append(["id", "denomination", "region", "year", "currency", "metal", "diameter"])
@@ -27,15 +33,6 @@ def add_new_data(denomination, region, year, currency, metal, diameter):
 def overwrite_data(id, denomination, region, year, currency, metal, diameter):
     data[id] = [id, denomination, region, year, currency, metal, diameter]
 
-
-@app.route("/show")
-def show():
-    file = open("static/coin_database.txt")
-    content = file.read()
-    coins = json.loads(content)
-    denominations = [coin['denomination'] for coin in coins]
-    return content
-
 # parameters flask
 @app.route("/add", methods=['POST'])
 def add():
@@ -43,16 +40,13 @@ def add():
     add_new_data(*features)
     return {'response': "Coin added"}
 
-@app.route("/catalogue")
-def catalogue():
-    return render_template('catalogue.html')
-
-@app.route("/display_collection")
-def display_collection():
-    return render_template('display_collection.html')
-@app.route("/dummy")
-def dummy():
-    return 'DUMMY'
+@app.route("/edit", methods=['POST'])
+def edit():
+    features = request.get_json()
+    print(features)
+    serial = find_id(*features)
+    #overwrite_data(serial, *features)
+    return {'response': "Coin edited"}
 
 @app.route("/coins")
 def coins():
@@ -60,14 +54,6 @@ def coins():
     result = f.read()
     f.close()
     return result
-
-@app.route("/add_coin")
-def add_coin():
-    return render_template('add_coin.html')
-
-@app.route("/edit_coin")
-def edit_coin():
-    return render_template('edit_coin.html')
 
 if __name__ == '__main__':
    app.run()
