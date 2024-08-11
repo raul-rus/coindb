9c11fa5cf526e0b592571abda1607cda729b7d43
Raul Rus <raul.victor.rus@gmail.com>
2024-08-11 23:51:17 +0100

    Added search and table to catalogue.

---
+++ static/catalogue.js
@@ -0,0 +1,32 @@
+function makeTable(container, data) {
+    var table = $("<table/>");
+    $.each(data, function(rowIndex, r) {
+        var row = $("<tr/>");
+        $.each(r, function(colIndex, c) {
+            row.append($("<td/>").text(c));
+        });
+        table.append(row);
+    });
+    return container.html(table);
+}
+
+var current_coins = [];
+
+function callback(result_in) {
+    current_coins = result_in;
+    makeTable($("#coin_list"), result_in);
+}
+
+function update_search() {
+    query = $("#search").val();
+    results = [];
+    for (let i = 0; i < current_coins.length; i++) {
+        let coin = current_coins[i];
+        if (coin[2].match(query)) {
+            results.push(coin);
+        }
+    }
+    makeTable($("#coin_list"), results);
+}
+
+
